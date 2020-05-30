e12b8c445b50bef0ce5496529eadd24e277c9bcd
Zamur Vedovatto <vedovatto@gmail.com>
2020-05-30 20:11:03 -0300

    fetch data from adviceslip api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 export default class App extends Component {
   state = { advice: '' };
 
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = async () => {
+    await axios.get('https://api.adviceslip.com/advice')
+      .then(response => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
+    const { advice } = this.state;
     return (
-      <div>Aaaa</div>
+      <div>{advice}</div>
     )
   }
 }
