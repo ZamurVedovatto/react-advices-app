3cbea154dcaaa1ee6793c0fbce713e60a89ea611
Zamur Vedovatto <vedovatto@gmail.com>
2020-05-30 20:48:56 -0300

    add loading

--- src/App.js
+++ src/App.js
@@ -3,28 +3,38 @@ import axios from 'axios';
 import './App.css';
 
 export default class App extends Component {
-  state = { advice: '' };
+  state = { 
+    advice: '',
+    isLoading: false
+  };
 
   componentDidMount() {
     this.fetchAdvice();
   }
 
   fetchAdvice = async () => {
+    this.setState({ isLoading: true, advice: '' });
     await axios.get('https://api.adviceslip.com/advice')
       .then(response => {
         const { advice } = response.data.slip;
         this.setState({ advice });
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
+    this.setState({ isLoading: false });
   }
   
   render() {
-    const { advice } = this.state;
+    const { advice, isLoading } = this.state;
     return (
       <div className="app">
-        <div className="card">
-          <h3 className="heading">{advice}</h3>
-          <button className="button" onClick={this.fetchAdvice}>
+        <div className="card">          
+          { 
+            advice === '' ?
+              <h5>Loading...</h5>
+              :
+              <h3 className="heading">{advice}</h3>
+          }     
+          <button className={`button ${ isLoading ? 'd-none': '' }`} onClick={this.fetchAdvice}>
             <span>Give me another advice</span>
           </button>
         </div>
